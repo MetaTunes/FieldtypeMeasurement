c73d52e685730cf518e0c83af356426e0a4acf42
Mark Evens <MetaTunes@users.noreply.github.com>
2022-04-17 19:14:21 +0100

    v0.0.12
    interactive dependent select in config, bug fixes

--- FieldtypeMeasurement.js
+++ FieldtypeMeasurement.js
@@ -15,6 +15,46 @@ function addTooltipClass() {
 document.addEventListener('DOMContentLoaded',() => {
     addTooltipClass();
 });
+
+/*
+We need to tell htmx that ProcessWire expects Ajax calls to send "X-Requested-With"
+ */
+
+let FieldtypeMeasurement = {
+    initHTMXXRequestedWithXMLHttpRequest: function () {
+        console.log("initHTMXXRequestedWithXMLHttpRequest - configRequest")
+        document.body.addEventListener("htmx:configRequest", (event) => {
+            event.detail.headers["X-Requested-With"] = "XMLHttpRequest"
+        })
+    },
+
+    listenToHTMXRequests: function () {
+        // before send - Just an example to show you where to hook BEFORE SEND htmx request
+        htmx.on("htmx:beforeSend", function (event) {
+            console.log("FieldtypeMeasurement - listenToHTMXRequests - event", event);
+        })
+    },
+}
+
+
+/**
+ * DOM ready
+ *
+ */
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (typeof htmx !== "undefined") {
+        // CHECK THAT htmx is available
+        console.log("HTMX!")
+        // init htmx with X-Requested-With
+        FieldtypeMeasurement.initHTMXXRequestedWithXMLHttpRequest()
+        // just for testing
+        FieldtypeMeasurement.listenToHTMXRequests()
+    } else {
+        console.log("NO HTMX!")
+    }
+})
+
+
 /*
 NOT IMPLEMENTED AT PRESENT - 2 PROBLEMS:
 1. element has no id or header so dragging kills resizing
